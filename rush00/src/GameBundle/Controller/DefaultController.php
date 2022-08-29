fb97037fa7896e598fe53ca86f39a6715411eb2d
Yohann Dana <yodana@c1r7p9.42quebec.com>
2022-08-29 17:45:39 -0400

    rush loading

--- rush00/src/GameBundle/Controller/DefaultController.php
+++ rush00/src/GameBundle/Controller/DefaultController.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace GameBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpClient\HttpClient;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    const apikey = '76c88b32';
+
     /**
      * @Route("/options/")
      */
@@ -20,7 +26,12 @@ class DefaultController extends Controller
      */
     public function getNewGame()
     {
-        
+        $client = HttpClient::create();
+        $response = $client->request(
+            'GET',
+            'https://www.omdbapi.com/?i=tt3896198&apikey=' . $this::apikey
+        );
+        var_dump($response->getContent());
         return $this->render('GameBundle:Default:index.html.twig');
     }
 }
